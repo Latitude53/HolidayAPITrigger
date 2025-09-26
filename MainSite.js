106e1c89246bf88b8aa29fedab05355022b15eaa
Latitude53 <jonny.bolton16@gmail.com>
2025-09-26 15:33:06 +0100

    Update MainSite.js
    Major flow reconstruction

--- MainSite.js
+++ MainSite.js
@@ -1,31 +1,42 @@
 function escapeHTML(str) {
-  return str.replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;');
+  return str?.replace(/&/g, '&amp;')
+             .replace(/</g, '&lt;')
+             .replace(/>/g, '&gt;')
+             .replace(/"/g, '&quot;')
+             .replace(/'/g, '&#39;');
 }
 
 // text to display
-const successString = 'You may now close this window.';
-const notFoundString = 'It may have been deleted.';
-const noLongerPendingString = 'Contact your system administrator to change this.';
-let otherFailedString = `<b>Something went wrong and the holiday request was not {newStatus}.</b><br>Please refresh the page to try again, or contact your system administrator if the problem persists.`;
-let newStatusSafe;
+const success = 'You may now close this window.';
+const notFound = 'It may have been deleted.';
+const forbidden = 'You are not the approver on record.';
+let noLongerPending = 'It is currently "{status}".';
+let modified = 'Click {here} to view the up-to-date request in the app.';
+let otherFailedString = `<b>Something went wrong and the holiday request was not {approvalResult}.</b><br>Please refresh the page to try again, or contact your system administrator if the problem persists.`;
 
 function getResponseString(response, prefix = '') {
   const code = response?.code;
-  const message = response?.message;
+  const message = escapeHTML(response?.message);
   const ID = response?.ID;
+  const status = escapeHTML(response?.status);
+  const link = 'icanhazdadjoke.com';
   prefix = prefix ? `${prefix}:<br>` : '';
   switch (code) {
-    case 200: return `${prefix}<b>${message}.</b><br>${successString}`;
+    case 200: return `${prefix}<b>${message}.</b><br>${success}`;
     case 404:
       console.warn(`Request not found (ID: ${ID})`);
-      return `${prefix}<b>${message}.</b><br>${notFoundString}`;
+      return `${prefix}<b>${message}.</b><br>${notFound}`;
+    case 403:
+      console.warn(`Insufficient permissions (ID: ${ID})`);
+      return `${prefix}<b>${message}.</b><br>${forbidden}`;
     case 409:
       console.warn(`Request not pending (ID: ${ID})`);
-      return `${prefix}<b>${message}.</b><br>${noLongerPendingString}`;
+      noLongerPending = noLongerPending.replace('{status}', status);
+      return `${prefix}<b>${message}.</b><br>${noLongerPending}`;
+    case 412:
+      console.warn(`Request modified (ID: ${ID})`);
+      modified = modified.replace('{here}', `<a href="${link}">here</a>`);
+      return `${prefix}<b>${message}.</b><br>${modified}`;
     default:
       console.warn(`Unhandled code ${code}`);
       console.log(response);
@@ -34,21 +45,21 @@ function getResponseString(response, prefix = '') {
 }
 
 window.onload = function() {
-  // extract parameters from URL
+  // extract decoded parameters from URL
   const params = new URLSearchParams(window.location.search);
-  const apiUrl = decodeURIComponent(params.get('apiUrl')); // this is encoded in Power Apps
-  const itemIDsRaw = params.get('itemIDs');
-  const itemIDsArray = itemIDsRaw.split(',').map(Number);
-  const newStatusRaw = params.get('status');
-  newStatusSafe = escapeHTML((newStatusRaw || '').toLowerCase());
-  otherFailedString = otherFailedString.replace('{newStatus}', newStatusSafe);
+  const flowUrl = params.get('flowUrl');
+  const requestType = params.get('requestType');
+  const approvalResultRaw = params.get('approvalResult');
+  const approvalResultSafe = escapeHTML((approvalResultRaw || '').toLowerCase());
+  otherFailedString = otherFailedString.replace('{approvalResult}', approvalResultSafe);
+  const requests = JSON.parse(params.get('requests'));
 
   // do a GET on the URL to trigger the flow and change the text displayed on the site
-  fetch(apiUrl,
+  fetch(flowUrl,
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ itemIDs: itemIDsArray, status: newStatusRaw })
+      body: JSON.stringify({ requestType: requestType, approvalResult: approvalResultRaw, requests: requests })
     }
   )
   .then(response => response.json().then(body => {
